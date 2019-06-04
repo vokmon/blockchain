d91e5b337b3d28c9c5b77de13e46934ed83c3fc2
Arnon Ruangthanawes <arnon@precisionsolutions.co.nz>
2019-06-04 21:02:58 +0700

    Compile, Migrate and Deploy to Ganache

--- eth-todo-list/contracts/TodoList.sol
+++ eth-todo-list/contracts/TodoList.sol
@@ -1,5 +1,9 @@
 pragma solidity ^0.5.0;
 
 contract TodoList {
-  uint taskCount = 0;
+  uint public taskCount = 1;
+
+  function getTaskCount() public view returns(uint) {
+    return taskCount;
+  }
 }
