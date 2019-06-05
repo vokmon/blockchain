67194f64d88fb3334921618abfe92e388a34f360
Arnon Ruangthanawes <arnon@precisionsolutions.co.nz>
2019-06-05 19:11:27 +0700

    Create task method in the contract and migrate to the server

--- eth-todo-list/contracts/TodoList.sol
+++ eth-todo-list/contracts/TodoList.sol
@@ -1,9 +1,31 @@
 pragma solidity ^0.5.0;
 
 contract TodoList {
-  uint public taskCount = 1;
+  uint public taskCount = 0;
 
-  function getTaskCount() public view returns(uint) {
-    return taskCount;
+  struct Task {
+    // unique id of the struct
+    uint id;
+
+    // text of task
+    string content;
+
+    // status of the task
+    // true -> complete/finished
+    // false -> unfishied task
+    bool completed;
+  }
+
+  // storage on the blockchain
+  mapping(uint => Task) public tasks;
+
+  constructor() public {
+    // default task to do
+    createTask("Check out dappuniversity.com");
+  }
+
+  function createTask(string memory _content) public {
+    taskCount++;
+    tasks[taskCount] = Task(taskCount, _content, false);
   }
 }
