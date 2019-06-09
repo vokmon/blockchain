622dddb8661ca8fa8558360c60bea6393ba102fa
Arnon Ruangthanawes <arnon@precisionsolutions.co.nz>
2019-06-09 14:32:23 +0700

    Fetch to do list from the blockchain

--- eth-todo-list-react/src/App.js
+++ eth-todo-list-react/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
 import Web3 from 'web3';
-
+import { TODO_LIST_ABI, TODO_LIST_ADDRESS } from './config'
 class App extends Component {
 
   constructor(props) {
     super(props)
-    this.state = { account: '', network: '' }
+    this.state = { account: '', network: '', tasks:[] }
   }
 
   componentWillMount() {
@@ -19,14 +19,60 @@ class App extends Component {
     const accounts = await web3.eth.getAccounts();
     const network = await web3.eth.net.getNetworkType();
     this.setState( {account: accounts[0], network});
+
+    const todoList = new web3.eth.Contract(TODO_LIST_ABI, TODO_LIST_ADDRESS);
+    this.setState({ todoList });
+    const taskCount = await todoList.methods.taskCount().call()
+    this.setState({ taskCount })
+    for (var i = 1; i <= taskCount; i++) {
+      const task = await todoList.methods.tasks(i).call()
+      this.setState({
+        tasks: [...this.state.tasks, task]
+      })
+    }
   }
 
   render() {
     return (
-      <div className="container">
-        <h1>Hello world!!</h1>
-        <p>Your account is {this.state.account} from {this.state.network} network</p>
+    <div>
+    <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+      <a className="navbar-brand col-sm-3 col-md-2 mr-0" href="http://www.dappuniversity.com/free-download" target="_blank">Dapp University | Todo List</a>
+      <ul className="navbar-nav px-3">
+        <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
+          <small><a className="nav-link" href="#"><span id="account"><p>Your account is {this.state.account} from {this.state.network} network</p></span></a></small>
+        </li>
+      </ul>
+    </nav>
+    <div className="container-fluid">
+      <div className="row">
+        <main role="main" className="col-lg-12 d-flex justify-content-center">
+          <div id="loader" className="text-center">
+            <p className="text-center">Loading...</p>
+          </div>
+          <div id="content">
+            <form>
+              <input id="newTask" type="text" className="form-control" placeholder="Add task..." required />
+              <input type="submit" hidden="" />
+            </form>
+            <ul id="taskList" className="list-unstyled">
+              { this.state.tasks.map((task, key) => {
+                return(
+                  <div className="taskTemplate" className="checkbox" key={key}>
+                    <label>
+                      <input type="checkbox" />
+                      <span className="content">{task.content}</span>
+                    </label>
+                  </div>
+                )
+              })}
+            </ul>
+            <ul id="completedTaskList" className="list-unstyled">
+            </ul>
+          </div>
+        </main>
       </div>
+    </div>
+  </div>
     );
   }
 }
---
+++ eth-todo-list-react/src/config.js
@@ -0,0 +1,117 @@
+export const TODO_LIST_ADDRESS = '0x7E890A2159b1E0276BAeac1256E877070e638b13';
+export const TODO_LIST_ABI = [
+  {
+    "constant": true,
+    "inputs": [
+      {
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "name": "tasks",
+    "outputs": [
+      {
+        "name": "id",
+        "type": "uint256"
+      },
+      {
+        "name": "content",
+        "type": "string"
+      },
+      {
+        "name": "completed",
+        "type": "bool"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "taskCount",
+    "outputs": [
+      {
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "constructor"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": false,
+        "name": "id",
+        "type": "uint256"
+      },
+      {
+        "indexed": false,
+        "name": "content",
+        "type": "string"
+      },
+      {
+        "indexed": false,
+        "name": "completed",
+        "type": "bool"
+      }
+    ],
+    "name": "TaskCreated",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": false,
+        "name": "id",
+        "type": "uint256"
+      },
+      {
+        "indexed": false,
+        "name": "completed",
+        "type": "bool"
+      }
+    ],
+    "name": "TaskCompleted",
+    "type": "event"
+  },
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "name": "_content",
+        "type": "string"
+      }
+    ],
+    "name": "createTask",
+    "outputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [
+      {
+        "name": "_id",
+        "type": "uint256"
+      }
+    ],
+    "name": "toggleCompleted",
+    "outputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  }
+];
