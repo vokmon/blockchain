bd81023c5ffef5c2d74c250a7513db630e374bb8
Arnon Ruangthanawes <arnon@precisionsolutions.co.nz>
2019-06-09 13:51:30 +0700

    Fetch data from web3js

--- eth-todo-list-react/src/App.js
+++ eth-todo-list-react/src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Web3 from 'web3';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = { account: '', network: '' }
+  }
+
+  componentWillMount() {
+    this.loadBlockchainData();
+  }
+
+  async loadBlockchainData() {
+    const address = 'http://localhost:7545';
+    const web3 = new Web3(new Web3.providers.HttpProvider(address));
+    const accounts = await web3.eth.getAccounts();
+    const network = await web3.eth.net.getNetworkType();
+    this.setState( {account: accounts[0], network});
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <h1>Hello world!!</h1>
+        <p>Your account is {this.state.account} from {this.state.network} network</p>
+      </div>
+    );
+  }
 }
 
 export default App;
